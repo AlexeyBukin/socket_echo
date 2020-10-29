5b3de2671c89c284db6576c8cdcd8a11f453e25c
Alex <alexey.green.28@gmail.com>
2020-10-30 01:45:45 +0300

    updated server code: unstable

--- src/server/server.c
+++ src/server/server.c
@@ -6,7 +6,7 @@
 /*   By: kcharla <kcharla@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/28 22:49:53 by kcharla           #+#    #+#             */
-/*   Updated: 2020/10/29 05:48:26 by hush             ###   ########.fr       */
+/*   Updated: 2020/10/30 01:36:15 by kcharla          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -16,12 +16,21 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <memory.h>
+#include <poll.h>
 
 // copy-paste from "https://jameshfisher.com/2017/04/05/set_socket_nonblocking/"
 // but this is the first right thing after hours of googling so ill leave it as is
 // sorry
 
 int guard(int n, char * err) { if (n == -1) { perror(err); exit(1); } return n; }
+
+int		client_disconnect()
+{
+
+}
+
+char 		*server_process_client_line(char *cline);
 
 int main() {
 	int listen_socket_fd = guard(socket(AF_INET, SOCK_STREAM, 0), "could not create TCP listening socket");
@@ -32,23 +41,187 @@ int main() {
 	addr.sin_port = htons(8080);
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	guard(bind(listen_socket_fd, (struct sockaddr *) &addr, sizeof(addr)), "could not bind");
-	guard(listen(listen_socket_fd, 100), "could not listen");
-	for (;;) {
-		int client_socket_fd = accept(listen_socket_fd, NULL, NULL);
-		if (client_socket_fd == -1) {
-			if (errno == EWOULDBLOCK) {
-				printf("No pending connections; sleeping for one second.\n");
-				sleep(1);
-			} else {
-				perror("error when accepting connection");
-				exit(1);
+	guard(listen(listen_socket_fd, 1), "could not listen");
+
+	int client_socket_fd = -1;
+	int has_client = 0;
+	char welcome_msg[] = "Server is ready. Type 'exit' to close connection.\n";
+	char error_msg[] = "Some error caused server to close connection. Press 'Enter' to continue.\n";
+	char *client_str = NULL;
+	char *client_line = NULL;
+	char *server_line = NULL;
+	char client_buff[256];
+	int client_recv = 0;
+	int client_str_size = 0;
+	int client_str_old_size = 0;
+
+	while (1)
+	{
+		//try to get new client
+		//just a quick check, if no pending connections just continue the loop
+		if (!has_client)
+		{
+			client_socket_fd = accept(listen_socket_fd, NULL, NULL);
+			if (client_socket_fd == -1)
+			{
+				if (errno == EWOULDBLOCK)
+				{
+					printf("No pending connections right now.\n");
+				}
+				else
+				{
+					perror("error when accepting connection");
+					exit(1);
+				}
 			}
-		} else {
-			char msg[] = "hello\n";
-			printf("Got a connection; writing 'hello' then closing.\n");
-			send(client_socket_fd, msg, sizeof(msg), 0);
-			close(client_socket_fd);
+			else
+			{
+				has_client = 1;
+				send(client_socket_fd, welcome_msg, sizeof(welcome_msg), 0);
+				printf("Got a connection; writing welcome message.\n");
+			}
+		}
+
+		// check if client socket has data and get it
+		// non-blocking as well
+		if (has_client)
+		{
+			struct pollfd pfd;
+			pfd.fd = client_socket_fd;
+			pfd.events = POLLIN | POLLHUP | POLLRDNORM;
+			pfd.revents = 0;
+			if (poll(&pfd, 1, 0) > 0) {
+				client_recv = recv(client_socket_fd, client_buff, sizeof(client_buff) - 1, MSG_PEEK | MSG_DONTWAIT);
+				if (client_recv == 0) {
+					// poll() tells that socket changed
+					// recv() tell there is no data on socket
+					// that means that connection has been closed by client with no error
+
+					has_client = 0;
+					close(client_socket_fd);
+
+					free(client_str);
+					client_str = NULL;
+					client_str_size = 0;
+					client_str_old_size = 0;
+				}
+				else if (client_recv > 0)
+				{
+					// read all available data into string 'client_str'
+					// mark that new data available
+					// client_str_size = client_old_size + n;
+					while ((client_recv = read(client_socket_fd, client_buff, sizeof(client_buff) - 1)) > 0)
+					{
+						client_buff[client_recv] = '\0';
+						unsigned long old_size = 0;
+						if (client_str != NULL)
+							old_size += strlen(client_str);
+						char *new_str;
+						if ((new_str = (char *)malloc(old_size + client_recv + 1)) == NULL)
+						{
+							send(client_socket_fd, error_msg, sizeof(error_msg), 0);
+							close(client_socket_fd);
+							printf("%s\n", "Server process line error. Connection is closed, shutting down...");
+							exit(1);
+						}
+						*new_str = '\0';
+						if (client_str != NULL)
+							new_str = strcpy(new_str, client_str);
+						new_str = strcat(new_str, client_buff);
+						free(client_str);
+						client_str = new_str;
+					}
+					if (client_recv < 0)
+					{
+						//error on read from fd
+						//try to send error_msg and close connection
+						send(client_socket_fd, error_msg, sizeof(error_msg), 0);
+						printf("%s\n", "Server process line error. Connection is closed, shutting down...");
+
+						has_client = 0;
+						close(client_socket_fd);
+
+						free(client_str);
+						client_str = NULL;
+						client_str_size = 0;
+						client_str_old_size = 0;
+					}
+				}
+				else
+				{
+					//recv() error occured
+					if (errno == ECONNRESET)
+					{
+						//client failed
+						//but we should stand
+						has_client = 0;
+						close(client_socket_fd);
+
+						free(client_str);
+						client_str = NULL;
+						client_str_size = 0;
+						client_str_old_size = 0;
+					}
+					else
+					{
+						// we failed
+						close(client_socket_fd);
+						printf("%s\n", "Server error on recv()");
+						exit(0);
+					}
+				}
+			}
+		}
+
+		// proceed data that changed
+		// *** blocking part ***
+		if (has_client && client_str_size > client_str_old_size)
+		{
+			int i = client_str_old_size;
+			int line_begin = 0;
+			while (i < client_str_size)
+			{
+				if (client_str[i] == '\n')
+				{
+					client_line = strndup(client_str + line_begin, i);
+					line_begin = i + 1;
+
+					server_line = server_process_client_line(client_line);
+
+					free(client_line);
+					client_line = NULL;
+					if (server_line == NULL)
+					{
+						// some serious error occured, must close
+						send(client_socket_fd, error_msg, sizeof(error_msg), 0);
+						close(client_socket_fd);
+						printf("%s\n", "Server process line error. Connection is closed, shutting down...");
+						exit(1);
+					}
+					send(client_socket_fd, server_line, strlen(server_line), 0);
+				}
+				i++;
+			}
+			// update client_str so it has no '\n' anymore
+			char *tmp = strdup(client_str + line_begin);
+			free(client_str);
+			client_str = tmp;
 		}
 	}
 	return EXIT_SUCCESS;
+}
+
+char 		*server_process_client_line(char *cline)
+{
+	char	*res;
+
+	if (strcmp(cline, "exit\n") == 0)
+	{
+		res = strdup("Closing connection. Have a nice day :)\n");
+	}
+	else
+	{
+		res = strdup("Unknown command. Try again.\n");
+	}
+	return (res);
 }
